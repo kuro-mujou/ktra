49fb2dc9cbd61c2961a78a04b772d3d83a26647c
kuro-mujou <mguyenmanhtuan@gmail.com>
2024-07-27 12:28:18 +0700

    Update DatabaseConnect.cs

--- IS385SC_NguyenManhTuan_2166/DatabaseConnect.cs
+++ IS385SC_NguyenManhTuan_2166/DatabaseConnect.cs
@@ -42,6 +42,80 @@ namespace IS385SC_NguyenManhTuan_2166
             }
             return dataTable;
         }
+        public int xuly(string sql) 
+         { 
+             int kq = 0; 
+             try 
+             { 
+                 Connect(); 
+                 SqlCommand cmd = new SqlCommand(sql, con); 
+                 kq = cmd.ExecuteNonQuery();//thuc thi cau len khong can truy van  
+             } 
+             catch 
+             { 
+                 kq = 0; 
+             } 
+             finally 
+             { 
+                 Disconnect(); 
+             } 
+             return kq; 
+         }
+
+        // them vao cho btn them sua xoa
+        protected void btnThem_Click(object sender, EventArgs e) 
+        { 
+            TextBox txtmaloai = (TextBox)GridView1.FooterRow.FindControl("txtmaloai");  
+            TextBox txttenloai = (TextBox)GridView1.FooterRow.FindControl("txttenloai");  
+            string maloai=txtmaloai.Text; 
+            string tenloai=txttenloai.Text; 
+            int kq = kn.xuly("insert into Loai values ('" + maloai + "', '" + tenloai +  "')"); 
+        if (kq > 0)//neu cap nhat duoc thi hien thong bao 
+        { 
+            Response.Write("<script>alert('cap nhat thanh công');</script>");  GridView1.DataSource = kn.laydata("SELECT Loai.* FROM Loai");  GridView1.DataBind(); 
+        } 
+        else 
+        { 
+            Response.Write("<script>alert('cap nhat không thanh công');</script>");  } 
+        } 
+        //- Xử lý cho chức năng Xóa: 
+        //Kích chọn GridView -> vào properties -> chọn nút Events, tìm đến thuộc tính RowDeleting  nhấp chuôt và enter 
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)  { 
+            string maloai = e.Values["MaLoai"].ToString();
+            int kq = kn.capnhat("delete from Loai where MaLoai = "+ maloai);  if (kq > 0)//neu cap nhat duoc thi hien thong bao 
+        { 
+            Response.Write("<script>alert('Xóa thanh công');</script>");  GridView1.DataSource = kn.laydata("SELECT Loai.* FROM Loai");  GridView1.DataBind(); 
+        } 
+        else 
+        { 
+            Response.Write("<script>alert('Xóa không thanh công');</script>");  } 
+        } 
+        //Tương tự xử lý cho sự kiện sửa 
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)  { 
+            GridView1.EditIndex = e.NewEditIndex; 
+            GridView1.DataSource = abc.laydata("SELECT Loai.* FROM Loai");  GridView1.DataBind(); 
+        } 
+        //////////////////////////////////////////////////// 
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)  { 
+            GridView1.EditIndex = -1;//không lấy giá trị cột nào hết 
+            GridView1.DataSource = kn.laydata("SELECT Loai.* FROM Loai");  GridView1.DataBind(); 
+        } 
+        
+        //////////////////////////////////////////////////////////// 
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)  { 
+            string maloai = e.NewValues["MaLoai"].ToString(); 
+            string tenloai = e.NewValues["TenLoai"].ToString(); 
+            int kq = kn.capnhat("update Loai set MaLoai= '"+ maloai +"', TenLoai='"+  tenloai +"' where MaLoai='"+ maloai +"'"); 
+        if (kq > 0)//neu cap nhat duoc thi hien thong bao 
+        { 
+            Response.Write("<script>alert('Cập nhật thanh công');</script>");  GridView1.DataSource = kn.laydata("SELECT Loai.* FROM Loai");  GridView1.EditIndex = -1; 
+            GridView1.DataBind(); 
+        } 
+        else 
+        { 
+            Response.Write("<script>alert('Cập nhật không thanh công');</script>");  } 
+        }
+
     }
 }
 
