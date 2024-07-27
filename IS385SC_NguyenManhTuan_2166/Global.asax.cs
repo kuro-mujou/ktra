e8da893047ee8ebe4c4be9f66333750e4c8453bb
kuro-mujou <mguyenmanhtuan@gmail.com>
2024-07-27 12:32:31 +0700

    Update Global.asax.cs

--- IS385SC_NguyenManhTuan_2166/Global.asax.cs
+++ IS385SC_NguyenManhTuan_2166/Global.asax.cs
@@ -66,3 +66,42 @@ namespace IS385SC_NguyenManhTuan_2166
         }
     }
 }
+
+//code lab
+            //lblSoLuotTC.Text = Application["SoLuotTruyCap"].ToString();
+            //lblOnline.Text = Application["SLOnline"].ToString();
+//void Application_Start(object sender, EventArgs e)
+//{
+//    // Code that runs on application startup
+//    Application.Lock();
+//    RouteConfig.RegisterRoutes(RouteTable.Routes);
+//    BundleConfig.RegisterBundles(BundleTable.Bundles);
+//    //Kiểm tra xem có file Dem.txt
+//    //Nếu chưa có, tạo file
+//    if (!System.IO.File.Exists(Server.MapPath("~/Dem.txt")))
+//        System.IO.File.WriteAllText(Server.MapPath("~/Dem.txt"), "0");
+//    //Nếu đã có file Dem.txt, đọc số liệu người truy cập
+//    Application["SoLuotTruyCap"] =
+//    int.Parse(System.IO.File.ReadAllText(Server.MapPath("~/Dem.txt")));
+//    Application.UnLock();
+//}
+//void Session_Start(object sender, EventArgs e)
+//{
+//    // Code that runs when a new session is started
+//    //Tăng số lượt truy cập lên 1
+//    Application["SoLuotTruyCap"] =
+//    (int)Application["SoLuotTruyCap"] + 1;
+//    //Ghi xuống file
+//    System.IO.File.WriteAllText(Server.MapPath("~/Dem.txt"),
+//    Application["SoLuotTruyCap"].ToString());
+//    //Xử lý số người online
+//    //Nếu chưa có thì gán là 1, có rồi thì tăng 1
+//    if (Application["SLOnline"] == null)//chưa có
+//        Application["SLOnline"] = 1;
+//    else
+//        Application["SLOnline"] = (int)Application["SLOnline"] + 1;
+//}
+//void Session_End(object sender, EventArgs e)
+//{
+//    Application["SLOnline"] = (int)Application["SLOnline"] - 1;
+//}
